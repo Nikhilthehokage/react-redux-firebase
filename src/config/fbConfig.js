218573f826601bb89c75a64d28acfaeaca22aba3
Nikhilthehokage <51057230+Nikhilthehokage@users.noreply.github.com>
2020-12-08 17:52:22 +0530

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,14 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCx_XEMU8FMMd6p1fgHs97kbng5-B0f_2U",
-    authDomain: "sampleapp-e2461.firebaseapp.com",
-    databaseURL: "https://sampleapp-e2461.firebaseio.com",
-    projectId: "sampleapp-e2461",
-    storageBucket: "sampleapp-e2461.appspot.com",
-    messagingSenderId: "849331463893",
-    appId: "1:849331463893:web:c3e3ccf2c18858f1b8e45c",
-    measurementId: "G-XN86TDY30R"
+    // paste your firebase config
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
